7e61a29eab781ccc1be157c08960966949752270
taozeran <841070682@qq.com>
2020-01-09 04:40:18 +0800

    fix resource loading when package as jar

--- src/main/java/org/baeldung/spring/LoginNotificationConfig.java
+++ src/main/java/org/baeldung/spring/LoginNotificationConfig.java
@@ -3,10 +3,10 @@ package org.baeldung.spring;
 import com.maxmind.geoip2.DatabaseReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ResourceUtils;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import ua_parser.Parser;
 
-import java.io.File;
 import java.io.IOException;
 
 @Configuration
@@ -18,10 +18,10 @@ public class LoginNotificationConfig {
     }
 
     @Bean
-    public DatabaseReader databaseReader() throws IOException {
-        File database = ResourceUtils
-                .getFile("classpath:maxmind/GeoLite2-City.mmdb");
-        return new DatabaseReader.Builder(database)
+    public DatabaseReader databaseReader(ResourceLoader resourceLoader) throws IOException {
+        Resource resource = resourceLoader
+                .getResource("classpath:maxmind/GeoLite2-City.mmdb");
+        return new DatabaseReader.Builder(resource.getInputStream())
                 .build();
     }
 }
